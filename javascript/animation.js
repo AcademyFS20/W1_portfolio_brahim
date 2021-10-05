6fec92bc6e155067f614222088fd10d662e06e5a
belghiti-brahim <belghiti.brahim@pm.me>
2021-10-05 11:40:55 +0100

    adding_animaion-typewriter

---
+++ javascript/animation.js
@@ -0,0 +1,20 @@
+const txt_animation = document.querySelector('.txt-animation');
+console.log(txt_animation)
+
+var typewriter = new Typewriter(txt_animation, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2000)
+  .typeString('My name is Belghiti brahim')
+  .pauseFor(300)
+  .typeString('&nbsp I am a <strong>Web Designer</strong> ')
+  .deleteChars(14)
+  .typeString('<strong> Web developer </strong>')
+  .pauseFor(1000)
+  .start();
+
+
+
