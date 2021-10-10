591ad4fc344a2f4f81be7c0ad48bb677a5c4489b
belghiti-brahim <belghiti.brahim@pm.me>
2021-10-10 15:31:58 +0100

    adding_responcive

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,27 +1,27 @@
-const txt_animation = document.querySelector('.txt-animation');
-console.log(txt_animation)
+var app = document.querySelector('.txt-animation');
 
-var typewriter = new Typewriter(txt_animation, {
-    loop: true,
-    delay: 75,
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 55,
 });
 
 typewriter
-    .pauseFor(1200)
-    .typeString('My name is Belghiti brahim')
-    .pauseFor(300)
-    .typeString('&nbsp I am a <strong>Web Designer</strong> ')
-    .deleteChars(14)
-    .typeString('<strong> Web developer </strong>')
-    .pauseFor(1000)
-    .start();
+  .pauseFor(1200)
+  .typeString('My name is Belghiti brahim')
+  .pauseFor(300)
+  .typeString('<br> <strong>I am a <span style="color: #5067b3;">Web designer</span></strong> ')
+  .deleteChars(13)
+  .typeString('<strong><span style="color: #5067b3;">Web developer</span></strong>')
+  .pauseFor(1000)
+  .start();
+
+
 
 function myFunction() {
-    var x = document.getElementById("Nav-gauche");
-    if (x.className === "nav-gauche") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-gauche";
-    }
+  var x = document.getElementById("Nav-gauche");
+  if (x.className === "nav-gauche") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-gauche";
+  }
 }
-
